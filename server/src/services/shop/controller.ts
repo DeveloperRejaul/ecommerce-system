eb5cd083f468c6fbe89168a8611ad124e050d5fe
rejaul karim <devrejaul.official@gmail.com>
2024-08-24 16:31:20 +0600

    shop delete f added

--- server/src/services/shop/controller.ts
+++ server/src/services/shop/controller.ts
@@ -4,6 +4,8 @@ import { ShopService } from './service';
 import {
   Body,
   Controller,
+  Delete,
+  Param,
   Post,
   Request,
   UploadedFile,
@@ -16,15 +18,20 @@ import { CreateShopDto } from './dto';
 
 @Controller('api/v-1/shop')
 export class ShopController {
-  constructor(private readonly service: ShopService) {}
+  constructor(private readonly service: ShopService) { }
 
 
   @Post()
   @UseGuards(AuthGuard)
   @UseInterceptors(FileInterceptor('avatar'))
-  create(@Body() body:CreateShopDto, @UploadedFile() file, @Request() {id, email, role}) { 
-    return this.service.create(body, file, {id, email, role});
+  create(@Body() body: CreateShopDto, @UploadedFile() file, @Request() { id, email, role }) {
+    return this.service.create(body, file, { id, email, role });
   }
 
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  delete(@Param() param, @Request() { role, id }) {
+    return this.service.delete(param.id, role, id);
+  }
 
 }
--- server/src/services/shop/service.ts
+++ server/src/services/shop/service.ts
@@ -6,36 +6,48 @@ import { CreateShopDto } from './dto';
 import { AuthBody, IFileType } from 'src/types/types';
 import { UserRole } from '../user/schema';
 import { saveFile } from 'src/utils/file';
+import { UserService } from '../user/service';
 
 
 @Injectable()
 export class ShopService {
   constructor(
     @InjectModel(Shop.name) private model: Model<Shop>,
+    private userService: UserService
   ) { }
 
   /**
    * @description this function using for create shop
    * @returns  shop object
    */
-  async create(body: CreateShopDto, file: IFileType, auth:AuthBody) {
+  async create(body: CreateShopDto, file: IFileType, auth: AuthBody) {
     if (auth.role === UserRole.OWNER) {
-      if (file) { 
-        body['avatar'] = await saveFile(file);
-      }
+      if (file) body['avatar'] = await saveFile(file);
       return await this.model.create(body);
     }
     throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
   }
-    
-  async delete() { }
+
+
+  /**
+ * @description this function using for delete shop
+ * @returns  shop object
+ */
+  async delete(id: string, role: string, userId: string) {
+    if (role === UserRole.OWNER) return await this.model.findByIdAndDelete(id);
+    if (role === UserRole.SUPPER_ADMIN) {
+      const user = await this.userService.findById(userId);
+      if (user.shopId.toString() === id) {
+        return await this.model.findByIdAndDelete(id);
+      }
+    }
+    throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+  }
 
 
   async update() { }
 
-  async getAll() {
-
-  }
+  async getAll() { }
 
   async getByEmail() { }
 
